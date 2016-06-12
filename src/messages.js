80f94a1c35d763678348f5a3c3c551e01b8455ab
inuscript <inuscript@gmail.com>
2016-06-12 19:15:39 +0900

    cmp

--- src/messages.js
+++ src/messages.js
@@ -1,6 +1,9 @@
 const React = require('react')
-const { defaultProps, withProps } = require('recompose')
-
+const { compose, defaultProps, mapProps, withProps, renderComponent, createEagerComponent } = require('recompose')
+const recompose = require('recompose')
+const LevelMessage1 = require('./messages/level1')
+const LevelMessage2 = require('./messages/level2')
+console.log(recompose)
 const Message = ({background, color, msg}) => {
   let style = { 
     background, color,
@@ -33,7 +36,38 @@ const ErrorMsg = errorEnhance(Message)
 const warnEnhance = withProps({color: "#8a6d3b", background: "#fcf8e3"})
 const WarnMsg = warnEnhance(Message)
 
+const levelEnhancer = mapProps( (props) => {
+  let schema = {
+    error: {color: "#a94442", background: "#f2dede"},
+    warn: {color: "#8a6d3b", background: "#fcf8e3"}
+  }
+  if(schema[props.level]){
+    return Object.assign({}, props, schema[props.level])
+  }
+  return props
+})
+
+const LevelMsg = compose(messageEnhancer, levelEnhancer)(Message)
+
+const exampleEnhancer = (title, cmp) => {
+  const ErrSample = withProps({level: "error", msg: "level = error"})(cmp)
+  const WarnSample = withProps({level: "warn", msg: "level = warn"})(cmp)
+  const DefaultSample = withProps({msg: "level = null(default)"})(cmp)
+  const component = (
+    <div>
+      <div>{title}</div>
+      <ErrSample />
+      <WarnSample />
+      <DefaultSample />
+    </div>
+  )
+  // return functional stateless component
+  return () => component
+}
+
 const Main = () => {
+  let LevelMsgSample = exampleEnhancer("LevelMsg", LevelMsg)
+  let Sample1 = exampleEnhancer("sample1", LevelMessage1)
   return (<div>
     <DefaultMessage msg={"Default"} />
     <DefaultMessage msg={"Default with green"} color={"orange"} background={"purple"} />
@@ -41,6 +75,8 @@ const Main = () => {
     <DefaultMessageRaw msg={"Default with green"} color={"orange"} background={"purple"} />
     <ErrorMsg msg={"Error!!!"} />
     <WarnMsg msg={"Warn!!!"} />
+    <LevelMsgSample />
+    <Sample1 />
   </div>)
 }
 
