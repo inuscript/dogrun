bf8ff40065c505f8bc61f4f7fffb13941fa8237b
inuscript <inuscript@gmail.com>
2016-06-12 13:22:44 +0900

    recompose

--- src/index.js
+++ src/index.js
@@ -2,21 +2,31 @@
 const React = require('react')
 const ReactDom = require('react-dom')
 const { Component } = React
-const { branch } = require('recompose')
+const { branch, withProps } = require('recompose')
 
 const Message = ({background, color, msg}) => {
   let style = { 
     background, color,
     border: `${color} solid 1px`,
     borderRadius: 4,
-    padding: 10
+    padding: 10,
+    marginBottom: 5
   }
   return <div style={style}>{msg}</div>
 }
 
-const ErrorMsgRaw = ({msg}) => (<Message msg={msg} color={"#a94442"} background={"#f2dede"}/> )
+// const ErrorMsgRaw = ({msg}) => (<Message msg={msg} color={"#a94442"} background={"#f2dede"} /> )
+
+const errorEnhance = withProps({color: "#a94442", background: "#f2dede"})
+const ErrorMsg = errorEnhance(Message)
+
+const warnEnhance = withProps({color: "#8a6d3b", background: "#fcf8e3"})
+const WarnMsg = warnEnhance(Message)
 
 const Main = () => {
-  return <ErrorMsgRaw msg={"Error!!!"} />
+  return (<div>
+    <ErrorMsg msg={"Error!!!"} />
+    <WarnMsg msg={"Warn!!!"} />
+  </div>)
 }
 ReactDom.render(<Main />, document.getElementById('container'))
