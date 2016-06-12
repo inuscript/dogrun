84d912efc3de57d1903faa8d36a29fc33d8061ec
inuscript <inuscript@gmail.com>
2016-06-12 17:15:10 +0900

    add simple version

--- src/messages.js
+++ src/messages.js
@@ -15,10 +15,20 @@ const Message = ({background, color, msg}) => {
 const messageEnhancer = defaultProps({color: "#31708f", background: "#d9edf7"})
 const DefaultMessage = messageEnhancer(Message)
 
-// const ErrorMsgRaw = ({msg}) => (<Message msg={msg} color={"#a94442"} background={"#f2dede"} /> )
+// very simple 
+
+// very simple 
+const DefaultMessageRaw = (props) => {
+  const defaultProps = {color: "#31708f", background: "#d9edf7"}
+  const msgProps = Object.assign({}, defaultProps, props)
+  return (<Message {...msgProps} /> )
+}
 
 const errorEnhance = withProps({color: "#a94442", background: "#f2dede"})
 const ErrorMsg = errorEnhance(Message)
+
+// errorMsg(raw)
+// const ErrorMsg = ({msg}) => (<Message msg={msg} color={"#a94442"} background={"#f2dede"} /> )
 
 const warnEnhance = withProps({color: "#8a6d3b", background: "#fcf8e3"})
 const WarnMsg = warnEnhance(Message)
@@ -27,6 +37,8 @@ const Main = () => {
   return (<div>
     <DefaultMessage msg={"Default"} />
     <DefaultMessage msg={"Default with green"} color={"orange"} background={"purple"} />
+    <DefaultMessageRaw msg={"Default with green"}  />
+    <DefaultMessageRaw msg={"Default with green"} color={"orange"} background={"purple"} />
     <ErrorMsg msg={"Error!!!"} />
     <WarnMsg msg={"Warn!!!"} />
   </div>)
