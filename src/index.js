c34a7ffa134a7d8a76af25d32ac3c754df90bb03
inuscript <inuscript@gmail.com>
2016-06-12 16:58:56 +0900

    props

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 const React = require('react')
 const ReactDom = require('react-dom')
 const { Component } = React
-const { branch, withProps } = require('recompose')
+const { branch, defaultProps, withProps } = require('recompose')
 
-const Message = ({background, color, msg}) => {
+const RawMessage = ({background, color, msg}) => {
   let style = { 
     background, color,
     border: `${color} solid 1px`,
@@ -15,16 +15,21 @@ const Message = ({background, color, msg}) => {
   return <div style={style}>{msg}</div>
 }
 
+const messageEnhancer = defaultProps({color: "#31708f", background: "#d9edf7"})
+const Message = messageEnhancer(RawMessage)
+
 // const ErrorMsgRaw = ({msg}) => (<Message msg={msg} color={"#a94442"} background={"#f2dede"} /> )
 
-const errorEnhance = withProps({color: "#a94442", background: "#f2dede"})
+const errorEnhance = defaultProps({color: "#a94442", background: "#f2dede"})
 const ErrorMsg = errorEnhance(Message)
 
-const warnEnhance = withProps({color: "#8a6d3b", background: "#fcf8e3"})
+const warnEnhance = defaultProps({color: "#8a6d3b", background: "#fcf8e3"})
 const WarnMsg = warnEnhance(Message)
 
 const Main = () => {
   return (<div>
+    <Message msg={"Default"} />
+    <ErrorMsg msg={"Orange Error!!!"} color={"Orange"}/>
     <ErrorMsg msg={"Error!!!"} />
     <WarnMsg msg={"Warn!!!"} />
   </div>)
