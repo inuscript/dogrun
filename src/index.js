a82598b0f4f788f07795b4f5fb63d8285481016a
inuscript <inuscript@gmail.com>
2016-06-12 12:36:04 +0900

    simple

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ const ReactDom = require('react-dom')
 const { Component } = React
 const { branch } = require('recompose')
 
+const Message = ({background, color, msg}) => {
+  let style = { 
+    background, color,
+    border: `${color} solid 1px`,
+    borderRadius: 4,
+    padding: 10
+  }
+  return <div style={style}>{msg}</div>
+}
 
-const Main = () => <div>Hello</div>
+const Main = () => {
+  return <Message msg="Error! Error! Error!" color={"#a94442"} background={"#f2dede"}/>
+}
 ReactDom.render(<Main />, document.getElementById('container'))
