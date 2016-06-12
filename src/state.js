5ed3420729fe5215e8bcff6c0c8d40cdd2994253
inuscript <inuscript@gmail.com>
2016-06-12 21:22:32 +0900

    add

--- src/state.js
+++ src/state.js
@@ -1,11 +1,19 @@
 const React = require('react')
-const { withState } = require('recompose')
+const { withState, compose, withHandlers } = require('recompose')
 
-const Count = ({count, setCounter}) => {
-  let handle = () => setCounter(n => n + 1)
+const Count = ({count, onClick}) => {
   return <div>
-    <button onClick={handle} >{count}</button>
+    <button onClick={onClick} >{count}</button>
   </div>
 }
-const enhancer = withState('count', 'setCounter', 0)
+const enhancer = compose(
+  withState('count', 'setCounter', 0),
+  withHandlers({
+    onClick: ({count, setCounter}) => ev => {
+      setCounter(count => {
+        return count + 1
+      })
+    }
+  })
+)
 module.exports = enhancer(Count)
