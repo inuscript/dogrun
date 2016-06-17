647f36a7a6cd4151e27508ecf79b234c5df75ae6
inuscript <inuscript@gmail.com>
2016-06-17 22:29:44 +0900

    update v0.20

--- src/textcount/TextCounter.js
+++ src/textcount/TextCounter.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react'
+import mapProps from 'recompose/mapProps'
+import withHandlers from 'recompose/withHandlers'
+import compose from 'recompose/compose'
+// import { compose, defaultProps, mapProps, withProps, renderComponent, createEagerComponent } = require('recompose')
 
 const Text = ({text, count, onChange}) => {
   return (<div>
@@ -6,6 +10,18 @@ const Text = ({text, count, onChange}) => {
     <textarea value={text} onChange={onChange}></textarea>
   </div>)
 }
+
+const withCount = mapProps( ({text}) => {
+  return {
+    text: text,
+    count: text.length
+  }
+})
+let withChangeHandler = withHandlers({
+  onChange: (e) => {
+    this.setState({text: e.target.value})
+  }
+})
 
 export default class TextCounter extends Component{
   constructor(){
@@ -15,14 +31,21 @@ export default class TextCounter extends Component{
     }
   }
   render(){
-    return <Text 
-      text={this.state.text} 
-      count={this.state.text.length} 
-      onChange={ e => {
-        // console.log(e)
-        // console.log(e.target.value)
-        this.setState({text: e.target.value})
-      }}
-      />
+    let enhancer = compose(
+      withCount,
+      withChangeHandler
+    )
+    let wrap = enhancer(Text)
+    console.log(wrap)
+    return wrap({text: this.state.text})
+    // return <Text 
+    //   text={this.state.text} 
+    //   count={this.state.text.length} 
+    //   onChange={ e => {
+    //     // console.log(e)
+    //     // console.log(e.target.value)
+    //     this.setState({text: e.target.value})
+    //   }}
+    //   />
   }
 }
