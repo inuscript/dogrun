915a652827687744d58399bd14c9018766583a8d
inuscript <inuscript@gmail.com>
2016-06-29 21:25:51 +0900

    sample oversvable

--- src/rx/todo.js
+++ src/rx/todo.js
@@ -1,4 +1,28 @@
+import React from 'react'
+import componentFromStream from 'recompose/componentFromStream'
+import createEventHandler from 'recompose/createEventHandler'
+import { Observable } from '@reactivex/rxjs'
+const Counter = componentFromStream(props$ => {
+  const { handler: increment, stream: increment$ } = createEventHandler()
+  const { handler: decrement, stream: decrement$ } = createEventHandler()
+  const count$ = Observable.merge(
+      increment$.mapTo(1),
+      decrement$.mapTo(-1)
+    )
+    .startWith(0)
+    .scan((count, n) => count + n, 0)
+
+  return props$.combineLatest(
+    count$,
+    (props, count) =>
+      <div {...props}>
+        Count: {count}
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+  )
+})
 
 export default () => {
-  return <div>Todo</div>
+  return <Counter />
 }
