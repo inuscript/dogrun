ba48e7b5ba64ebfd6feacb341a4e04df862f0674
inuscript <inuscript@gmail.com>
2016-07-14 20:25:56 +0900

    setup

--- src/index.js
+++ src/index.js
@@ -1,20 +1,51 @@
 import React from 'react'
+import { createStore, combineReducers, applyMiddleware } from 'redux' 
 import { combineEpics, createEpicMiddleware } from 'redux-observable'
 import { connect, Provider } from 'react-redux'
 
-const pingEpic = action$ =>
-  action$.filter(action => action.type === 'PING')
+const INCREMENT = 'INCREMENT'
+
+const pingEpic = action$ => {
+  console.log(action$.ofType('PING'))
+  return action$.ofType('PING')
     .mapTo({ type: 'PONG' });
+}
+
 
 export const rootEpic = combineEpics(
   pingEpic
 )
-
 const epicMiddleware = createEpicMiddleware(rootEpic);
+const enhancer = applyMiddleware(epicMiddleware)
 
+const pingReducer = (state = 0, action) => {
+  switch(action.type){
+    case 'PING':
+      return 'PING'
+    case 'PONG':
+      return 'PONG'
+  }
+  return state
+}
+
+const reducer = combineReducers({ping: pingReducer})
+
+const store = createStore(reducer, {}, enhancer)
+
+const Counter = ({dispatch, ping}) => {
+  return (
+    <div>
+      {ping}
+      <button onClick={ dispatch({type: 'PING'}) }>ping</button>
+    </div>
+  )
+}
 
 export default () => {
+  let ConnectedCounter = connect()(Counter)
   return (
-    <div>rx</div>
+    <Provider store={store}>
+      <ConnectedCounter />
+    </Provider>
   )
 }
