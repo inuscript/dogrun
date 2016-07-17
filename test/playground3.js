9885e319d413914775cba7d1038675f78aa8c2a8
inuscript <inuscript@gmail.com>
2016-07-17 19:39:16 +0900

    scan

--- test/playground3.js
+++ test/playground3.js
@@ -2,41 +2,22 @@ import test from 'ava'
 import configureMockStore from 'redux-mock-store';
 import { createEpicMiddleware, combineEpics } from 'redux-observable';
 
-import 'rxjs/add/operator/mapTo'
-import 'rxjs/add/operator/map'
-import 'rxjs/add/operator/max'
-import 'rxjs/add/operator/combineAll'
-import 'rxjs/add/operator/last'
-import 'rxjs/add/operator/mergeMap'
-// import 'rxjs/add/operator/empty'
+import Rx from 'rxjs/Rx';
 
-
-
-test('Max', t => {
+test('scan', t => {
   const pingEpic = action$ => {
     return action$
       .ofType('PING')
-      .max((a, b) => {
+      .scan((a, b) => {
         let x = a.payload > b.payload ? a : b
         console.log("MM", a,b, x)
-        return x
-      })
-      .map( a => {
-        console.log("||||")
-        console.log(a)
+        // console.log(x)
         return {
-          type: 'PONG',
-          payload: a.payload * 10
+          type: 'PONG_SCAN',
+          payload: x.payload
         }
-      })
+      }, {payload: 0})
 
-      // .map(a => {
-      //   console.log(a)
-      //   return {
-      //     type: 'PONG',
-      //     payload: a.payload
-      //   }
-      // })
   }
   const epicMiddleware = createEpicMiddleware(pingEpic);
   const mockStore = configureMockStore([epicMiddleware]);
@@ -50,4 +31,6 @@ test('Max', t => {
 
   console.log("===")
   console.log(store.getActions())
+  // store.dispatch({ type: 'PING', payload: 4 })
+
 })
