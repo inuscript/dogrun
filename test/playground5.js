560354fe6ce6699bccad4821a9f6e93635920543
inuscript <inuscript@gmail.com>
2016-07-18 21:50:54 +0900

    wip

--- test/playground5.js
+++ test/playground5.js
@@ -7,31 +7,38 @@ import Rx from 'rxjs/Rx';
 test('XXX', t => {
   const addComp = action$ => Observable.takeUntil(action$.ofType('ADD_COMPLETE'))
 
-  const pingEpic = action$ => action$
-    .ofType('ADD')
-    // .window(addComp(action$))
-    // .windowCount(3)
-    .takeUntil(action$.ofType('ADD_COMPLETE'))
-    // .windowWhen(action$.ofType('ADD_COMPLETE'))
-    .pluck('payload')
+  const pingEpic = action$ => {
+    const add = action$
+      .ofType('ADD')
+      .pluck('payload')
       .startWith(0)
       .reduce((acc, curr) => {
         console.log(acc, curr, "reduce")
         return acc + curr
       })
-    .map( sum => ({
-      type: 'PONG!!!',
-      payload: sum
-    }))
+      .map( sum => ({
+        type: 'PONG!!!',
+        payload: sum
+      }))
+    return action$
+      .window(action$.ofType('ADD_START'))
+        .takeUntil(action$.ofType('ADD_COMPLETE'))
+        .combineLatest(add)
+        // .pluck('payload')
+          
+  }
 
   const epicMiddleware = createEpicMiddleware(pingEpic);
   const mockStore = configureMockStore([epicMiddleware]);
 
   let store = mockStore()
+  store.dispatch({ type: 'ADD_START' })
   store.dispatch({ type: 'ADD', payload: 1 })
   store.dispatch({ type: 'ADD', payload: 2 })
   store.dispatch({ type: 'ADD_COMPLETE' })
+  store.dispatch({ type: 'ADD_START' })
   store.dispatch({ type: 'ADD', payload: 4 })
+  store.dispatch({ type: 'ADD', payload: 1 })
   store.dispatch({ type: 'ADD_COMPLETE' })
 
   console.log(store.getActions())
