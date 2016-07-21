da01ccfda47a7be45bc473aacc1355cdafe6a620
inuscript <inuscript@gmail.com>
2016-07-21 09:19:59 +0900

    realtime validate

--- src/validate/index.js
+++ src/validate/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { connect, Provider } from 'react-redux'
 
 const nameReducer = (state = '', action) => {
@@ -11,26 +11,31 @@ const nameReducer = (state = '', action) => {
   }
 }
 
+const reducer = combineReducers({
+  name: nameReducer
+})
+
 const changeName = (name) => ({
   type: 'CHANGE_NAME',
   payload: name
 })
 
-const store = createStore(nameReducer)
+const store = createStore(reducer, {})
 
 const Input = ({value, onChange}) => (
   <input value={value} onChange={onChange} />
 )
 
 const Errors = ({errors}) => {
-  if(!errors || errors.length){
+  if(!errors || errors.length === 0){
     return null
   }
-  const errs = errors.map( (err) => (<li>{err}</li>) )
-  return <ul style="background:red;color:white">{ errs }</ul>
+  const errs = errors.map( (err, i) => (<li key={err}>{err}</li>) )
+  const style = {background:"red", color:"white"}
+  return <ul style={style}>{ errs }</ul>
 }
 
-const validate = (validateFunction) => (BaseComponent) => {
+const withValidateError = (validateFunction) => (BaseComponent) => {
   return (props) => {
     const errors = validateFunction(props)
     const appendProps = {
@@ -39,18 +44,30 @@ const validate = (validateFunction) => (BaseComponent) => {
     return (
       <div>
         <Errors errors={errors} />
-        <BaseComponent {...props} />
+        <BaseComponent {...props} {...appendProps}/>
       </div>
     )
   }
 }
+
+const nameValidation = ({value}) => {
+  if(value.length < 4){
+    return ['name least 4 char']
+  }
+  if(value.length > 12){
+    return ['name less 12 char']
+  }
+  return []
+}
+
+const NameInput = withValidateError(nameValidation)(Input)
 
 const MainComponent = ({dispatch, name, errors}) => {
   return (
     <div>
       <div>
         <div>name: {name}</div>
-        <Input value={name} onChange={ e => dispatch(changeName(e.target.value))}/>
+        <NameInput value={name} onChange={ e => dispatch(changeName(e.target.value))}/>
       </div>
     </div>
   )
