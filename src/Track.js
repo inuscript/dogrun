42fd0e26172e76b3236fa97eb78930e7a1b99c64
inuscript <inuscript@gmail.com>
2016-08-28 15:45:53 +0900

    add

--- src/Track.js
+++ src/Track.js
@@ -14,19 +14,53 @@ import { toSteps, convertMML } from './mml'
 // convertMML("cdefedcr")
 // let song = toSteps([ "60", "62", "64"])
 
-export default () => {
-  // let song = convertMML("e g > e c d g")
-  // let song = convertMML("e g")
-  let song = toSteps([ "60", "62", "64"])
-  console.log(song)
+const MMLSynth = ({mml}) => {
+  let song = convertMML(mml)
+  return <Synth
+    type="triangle"
+    steps={song}
+  />
+}
+
+const Sound = ({mml, playing}) => {
   return (
-    <Song tempo={160} playing={true}>
+    <Song tempo={160} playing={playing}>
       <Sequencer resolution={8} bars={1}>
-        <Synth
-          type="triangle"
-          steps={song}
-        />
+        <MMLSynth mml={mml}/>
       </Sequencer>
     </Song>
   )
+}
+
+const Controller = ({mml, playing, onChangePlay, onChangeMML}) => {
+  return (
+    <div>
+      <button onClick={onChangePlay}>{playing ? "pause": "play" }</button>
+      <textarea onChangeMML={onChangeMML}>{mml}</textarea>
+    </div>
+  )
+}
+
+export default class MMLMusci extends Component{
+  state = {
+    mml: "e g > e c d g",
+    playing: false
+  }
+  handleTogglePlay = () => {
+    this.setState({playing: !this.state.playing})
+  }
+  handleChangeMML = (value) => {
+    this.setState({mml: value})
+  }
+  render(){
+    const props = {
+      ...this.state,
+      onChangePlay: this.handleTogglePlay,
+      onChangeMML: this.handleChangeMML
+    }
+    return (<div>
+      <Sound {...this.state}/>
+      <Controller {...props}/>
+    </div>)
+  }
 }
