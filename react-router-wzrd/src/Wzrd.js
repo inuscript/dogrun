decb4d486e9fb19a276624bd74b92f6893e1da65
inuscript <inuscript@gmail.com>
2016-10-06 22:10:06 +0900

    wip

--- react-router-wzrd/src/Wzrd.js
+++ react-router-wzrd/src/Wzrd.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 
-import Router from 'react-router/HashRouter'
+import Router from 'react-router/BrowserRouter'
 import { Match, Link } from 'react-router'
+
+class WizerdLink extends Component {
+  render(){
+    const props = this.props
+    return <Link to="/2" {...props}>{
+      ({isActive, location, href, onClick, transition}) => {
+        return <button onClick={onClick}>{props.children}</button>
+      }
+    }</Link>
+  }
+}
 
 const First = () => (
   <div>
     <div>First</div>
+    <Match pattern="/2" component={Second} />
     <Link to="/2">next</Link>
   </div>
 )
 const Second = () => (
   <div>
     <div>Second</div>
+    {/* <WizerdLink>next</WizerdLink> */}
+    <Match pattern="/3" component={Third} />
     <Link to="/3">next</Link>
   </div>
 )
@@ -21,8 +35,6 @@ export default () => {
   return <Router>
     <div>
       <Match pattern="/" exactly component={First} />
-      <Match pattern="/2" component={Second} />
-      <Match pattern="/3" component={Third} />
     </div>
   </Router>
 }
