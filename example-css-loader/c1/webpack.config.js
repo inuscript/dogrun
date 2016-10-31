18a7b164fc1d0ae222470cc603a7b0a0fda94d06
inuscript <inuscript@gmail.com>
2016-11-01 00:03:17 +0900

    wip

--- example-css-loader/c1/webpack.config.js
+++ example-css-loader/c1/webpack.config.js
@@ -5,12 +5,9 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.css/,
-      // loaders: ['style', 'css']
-      loader: 'css',
-      query: {
-        // root: '.'
-        // module: true
-      }
+      // loader: 'css',
+      loader: 'style!css?importLoaders=1',
+      loader: 'style!css',
     }],
   },
   output: {
