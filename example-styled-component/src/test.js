9946cfbab8f0407914ae5f4d75806aaa1f85e228
inuscript <inuscript@gmail.com>
2016-10-31 22:38:50 +0900

    wip

---
+++ example-styled-component/src/test.js
@@ -0,0 +1,26 @@
+// import styled from 'styled-components';
+
+const Button = styled.button`
+  background: white;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const Button = css`
+  background: white;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const a = html`<div>aaa</div>`
+
+
+export default Button;
