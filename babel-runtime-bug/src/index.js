ecf832323d0100947e52b30d5620d7c3b0234210
inuscript <inuscript@gmail.com>
2016-11-17 21:06:10 +0900

    wip

--- babel-runtime-bug/src/index.js
+++ babel-runtime-bug/src/index.js
@@ -1,16 +1,2 @@
-import React from 'react'
-class Foo extends React{
-  baz = "bee"
-  handleHoge = () => {
-    Object.assing({}, {a: "b"})
-    const b = {...a, ...c}
-    console.log(this)
-  }
-  render(){
-    return <div>Hello</div>
-  }
-}
-
-Object.keys({"r":"b"})
-Object.defineProperties({}, "foo", "zzz")
-export const foo = 'bar'
+import Mod from './mod'
+console.log(Mod)
