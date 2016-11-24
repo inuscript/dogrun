ed83901c5a02c5186aa13e027c23b9f4ce108bcc
inuscript <inuscript@gmail.com>
2016-11-24 20:50:17 +0900

    wip

--- example-to-sfc/src/App.js
+++ example-to-sfc/src/App.js
@@ -2,6 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const SfcInput = (props) => <input {...props} />
+const SfcInput2 = ({value, onChange}) => <input value={value} onChange={onChange} />
+const SfcInput3 = ({value, onChange}) => <input defaultValue={value} onChange={onChange} />
+const SfcInput4 = ({value, onChange}) => <input value={value} />
+
+class InputTest extends Component {
+  constructor(){
+    super()
+    this.state = {
+      inputVal: "hoge"
+    }
+  }
+  handleChange = (e) => {
+    this.setState({
+      inputVal: e.target.value
+    })
+  }
+  render(){
+    return <div>
+      {this.state.inputVal}
+      <SfcInput
+        value={this.state.inputVal}
+        onChange={this.handleChange}
+      />
+      <SfcInput2
+        value={this.state.inputVal}
+        onChange={this.handleChange}
+      />
+      <SfcInput3
+        value={this.state.inputVal}
+        onChange={this.handleChange}
+      />
+      <SfcInput4
+        value={this.state.inputVal}
+      />
+    </div>
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +49,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
+        <InputTest />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
