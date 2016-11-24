2f0266a0b0a35fb769d9d99299d28a4a8fc55124
inuscript <inuscript@gmail.com>
2016-11-24 22:08:47 +0900

    wip

--- example-router-atag/src/App.js
+++ example-router-atag/src/App.js
@@ -1,19 +1,60 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Match, Link } from 'react-router'
 import logo from './logo.svg';
 import './App.css';
+
+const Header = () => (
+  <div>
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>This is Header</h2>
+    </div>
+    <Menu />
+  </div>
+)
+
+const Menu = () => (
+  <ul>
+    <li><Link to='/dog1'>dog1(Link)</Link></li>
+    <li><a href='/dog1'>dog1(a)</a></li>
+  </ul>
+)
+
+const DogPage1 = () => (
+  <ul>
+    <li>This is Dog1</li>
+    <li><Link to='/dog2'>dog2</Link></li>
+    <li><a href='/cat'>cat</a></li>
+  </ul>
+)
+
+const DogPage2 = () => (
+  <ul>
+    <li>This is Dog2</li>
+    <li><Link to='/dog1'>dog1</Link></li>
+    <li><a href='/cat'>cat</a></li>
+  </ul>
+)
+
+const CatPage = () => (
+  <ul>
+    <li>This is Cat</li>
+    <li><a href='/dog1'>dog1</a></li>
+    <li><a href='/dog2'>dog2</a></li>
+  </ul>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Match pattern='/dog1' component={DogPage1} />
+          <Match pattern='/dog2' component={DogPage2} />
+          <Match pattern='/cat' component={CatPage} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </BrowserRouter>
     );
   }
 }
