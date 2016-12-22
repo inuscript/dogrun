ac296cea15f44ff4e7c5daa6fe34fc38989b5135
inuscript <inuscript@gmail.com>
2016-12-23 01:16:20 +0900

    add

--- rx-run/test/index.js
+++ rx-run/test/index.js
@@ -1,6 +1,7 @@
 require("rxjs")
 const { PromiseObservable } = require('rxjs/observable/PromiseObservable')
 
+const expect = require("expect")
 const axios = require("axios")
 const configureMockStore = require('redux-mock-store').default
 
@@ -36,27 +37,45 @@ const epicMiddleware = createEpicMiddleware(fetchUserEpic);
 const mockStore = configureMockStore([epicMiddleware]);
 
 
-describe('fetchUserEpic', () => {
-  let store;
+const assertAction = (targetEpic, dispatchAction, expectAction, cb) => {
+  const _epicMiddleware = createEpicMiddleware(targetEpic);
+  const _mockStore = configureMockStore([_epicMiddleware]);
 
-  beforeEach(() => {
-    store = mockStore();
-  });
-
-  afterEach(() => {
-    epicMiddleware.replaceEpic(fetchUserEpic);
-  });
-
-  it('produces the user model', (done) => {
-    store.dispatch({ type: FETCH_USER });
-    // store.dispatch({ type: "XXX" });
-    setTimeout( () => {
-      console.log(store.getActions())
-      done()
-    }, 0)
-    // expect(store.getActions()).toEqual([
-    //   { type: FETCH_USER },
-    //   { type: FETCH_USER_FULFILLED, payload }
-    // ])
+  const store = _mockStore();
+  store.dispatch(dispatchAction)
+  const unsubscribe = store.subscribe( () => {
+    expect(store.getActions()).toEqual(expectAction)
+    unsubscribe()
+    cb()
   })
+}
+
+describe('fetchUserEpic', () => {
+  it('produces the user model', (done) => {
+    const store = mockStore();
+    store.dispatch({ type: FETCH_USER })
+    const unsubscribe = store.subscribe( () => {
+      expect(store.getActions()).toEqual([
+        { type: FETCH_USER },
+        { type: FETCH_USER_FULFILLED, payload }
+      ])
+      unsubscribe()
+      done()
+    })
+  })
+  it('produces the user model', (done) => {
+    assertAction(fetchUserEpic, { type: FETCH_USER }, [
+      { type: FETCH_USER },
+      { type: FETCH_USER_FULFILLED, payload }
+    ], done)
+  })
+  // it('produces the user model', (done) => {
+  //   うまくいかないパターン。
+  // const store = mockStore();
+  //   store.dispatch({ type: FETCH_USER })
+  //   expect(store.getActions()).toEqual([
+  //     { type: FETCH_USER }, // こっちしか来ない
+  //     { type: FETCH_USER_FULFILLED, payload }
+  //   ])
+  // })
 })
