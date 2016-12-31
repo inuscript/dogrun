f097d20a3daa3487815b92cc39d4cfba45fb9a8b
inuscript <inuscript@gmail.com>
2016-12-31 22:15:21 +0900

    add

--- redux-sloth/src/App.js
+++ redux-sloth/src/App.js
@@ -5,9 +5,11 @@ import updeep from 'updeep'
 import { createAction } from 'redux-actions'
 
 const initialState = {
-  counter: 0
+  counter: 0,
+  todos: []
 }
 
+// God reducer
 const reducer = (state = initialState, { payload } ) => {
   if(typeof payload !== "object"){
     return state
@@ -15,22 +17,98 @@ const reducer = (state = initialState, { payload } ) => {
   return updeep(payload, state)
 }
 
+// counter
 const increment = createAction('INCREMENT', () => ({ counter: (i) => i + 1 }))
 const decrement = createAction('DECREMENT', () => ({ counter: (i) => i - 1 }))
-const actions = {
-  increment, decrement
-}
 
-const Counter = ({increment, decrement, counter}) => {
+const Counter = ({dispatch, counter}) => {
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
     </div>
   )
 }
-const CounterContainer = connect(state => state, actions)(Counter)
+
+let nextTodoId = 0
+const creteNewTodo = (text) => ({
+  text,
+  id: nextTodoId++,
+  completed: false
+})
+
+const addTodo = createAction('ADD_TODO', (text) => ({
+  todos: (todos) => {
+    return [ ...todos, creteNewTodo(text) ]
+  }
+}) )
+
+const toggleTodoComplete = (todo, id ) => {
+  if(todo.id !== id){
+    return todo
+  }
+  return Object.assign({}, todo, {
+    completed: !todo.completed
+  })
+
+}
+
+const toggleTodo = createAction('TOGGLE_TODO', (id) => ({
+  todos: (todos) => todos.map( todo => toggleTodoComplete(todo, id) )
+}) )
+
+const Todo = ({ onClick, completed, text, id }) => (
+  <li
+    onClick={onClick}
+    style={{ textDecoration: completed ? 'line-through' : 'none'}}
+  >
+    {id}: {text}
+  </li>
+)
+
+
+const TodoList = ({ todos, dispatch }) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => dispatch(toggleTodo(todo.id))}
+      />
+    )}
+  </ul>
+)
+const AddTodo = ({ dispatch }) => {
+  let input
+  return (
+    <div>
+      <form onSubmit={e => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        dispatch(addTodo(input.value))
+        input.value = ''
+      }}>
+        <input ref={node => {
+          input = node
+        }} />
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
+    </div>
+  )
+}
+
+const CounterContainer = connect(state => state)(Counter)
+const TodoListContainer = connect(state => state)(TodoList)
+const AddTodoContainer = connect(state => state)(AddTodo)
+const DebugContainer = connect(state => state)( (props) => {
+  console.log(props)
+  return null
+})
 
 class App extends Component {
   constructor(){
@@ -40,7 +118,13 @@ class App extends Component {
   render() {
     return (
       <Provider store={this.store}>
-        <CounterContainer />
+        <div>
+          <CounterContainer />
+          <hr />
+          <AddTodoContainer />
+          <TodoListContainer />
+          <DebugContainer />
+        </div>
       </Provider>
     );
   }
