08b36a25ef553d52a860a231422e14c82f3bfa83
inuscript <inuscript@gmail.com>
2017-01-18 19:44:42 +0900

    wip

--- eslint-semi-fix/.eslintrc.js
+++ eslint-semi-fix/.eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    "env": {
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  "env": {
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-extra-semi": 2
+  }
 };
