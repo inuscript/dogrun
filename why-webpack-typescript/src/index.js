b0e889eda28365d73b4005b5c7fc21b083fe3613
inuscript <inuscript@gmail.com>
2017-02-07 00:15:08 +0900

    wip

---
+++ why-webpack-typescript/src/index.js
@@ -0,0 +1,8 @@
+"use strict";
+const Roct = require("react");
+const react_1 = require("react");
+// import Reacd , {Component} from "react"
+console.log(Roct);
+console.log(react_1.default);
+// console.log(Reacd)
+// console.log(Component) 
---
+++ why-webpack-typescript/src/index.ts
@@ -0,0 +1,8 @@
+import * as Roct from "react"
+import Reac from "react"
+// import Reacd , {Component} from "react"
+
+console.log(Roct)
+console.log(Reac)
+// console.log(Reacd)
+// console.log(Component)
