6c129883f6d4e921bb2baf7661a8d5fc2e545ccf
inuscript <inuscript@gmail.com>
2017-02-19 17:12:26 +0900

    add

--- react-router-v4-redux/src/App.js
+++ react-router-v4-redux/src/App.js
@@ -1,21 +1,94 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*eslint no-console: 0*/
+import React from 'react'
+import { Redirect, BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider, connect } from 'react-redux'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+////////////////////////////////////////////////////////////
+// 1. reducer to keep the location in redux state
+
+// in a real redux app you'd want to use `window.location`
+// but for our demo we'll use this fake one
+const initialLocation = { pathname: '/', search: '', hash: '' }
+const locationReducer = (state = initialLocation, action) => {
+  return action.type === 'LOCATION_CHANGE' ?
+    action.location : state
 }
 
-export default App;
+
+////////////////////////////////////////////////////////////
+// normal redux stuff
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
+
+const store = applyMiddleware(logger)(createStore)(
+  combineReducers({ location: locationReducer })
+)
+
+const mapStateToAppProps = (state) => ({
+  location: state.location
+})
+
+
+////////////////////////////////////////////////////////////
+const App = connect(mapStateToAppProps)((props) => (
+  // 2. use Router as a controlled component, exactly like
+  //    controlling an input with redux state. Used this way
+  //    it doesn't listen to the history, and doesn't have
+  //    any of its own state, it only notifies the App that
+  //    the user is attempting to go to a new location, but
+  //    it won't go there. It goes to whichever location you
+  //    pass to it.
+  <BrowserRouter
+    history={props.history}
+    location={props.location}
+    onChange={(location) => {
+      // 3. Dispatch location changes
+      props.dispatch({
+        type: 'LOCATION_CHANGE',
+        location
+      })
+    }}
+  >
+    <div>
+    <ul>
+      <li><Link to="/one">One</Link></li>
+      <li><Link to="/two">Two</Link></li>
+      <li><Link to="/three">Three</Link></li>
+      <li><Link to="/four">Four</Link></li>
+      <li><Link to="/five">Five</Link></li>
+    </ul>
+    <Switch>
+      <Route path="/" exactly render={() => (
+        <div>
+          <p>Open the console to see the logger middleware.</p>
+        </div>
+      )}/>
+      <Route path="/one" render={() => <h3>One</h3>}/>
+      <Route path="/two" render={() => <h3>Two</h3>}/>
+      <Route path="/three" render={() => <h3>Three</h3>}/>
+      <Route path="/four" render={() => <h3>Four</h3>}/>
+      <Route path="/five" render={() => <h3>Five</h3>}/>
+    </Switch>
+    </div>
+  </BrowserRouter>
+))
+
+
+////////////////////////////////////////////////////////////
+// you don't need this history, it's passed in for the fake
+// browser window.
+const ReduxExample = ({ history }) => (
+  <Provider store={store}>
+    <App history={history}/>
+  </Provider>
+)
+
+export default ReduxExample
