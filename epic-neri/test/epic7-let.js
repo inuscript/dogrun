6d66f7604d831a87a67d85056b36bec12692e839
inuscript <inuscript@gmail.com>
2017-02-21 22:52:16 +0900

    add

---
+++ epic-neri/test/epic7-let.js
@@ -0,0 +1,48 @@
+require("rxjs")
+
+const { ActionsObservable, combineEpics, createEpicMiddleware } = require("redux-observable")
+const { Observable } = require("rxjs")
+const { startConnection, finishConnection, patchAction, fullfiledAction } = require("../actions")
+const { patchApi } = require("../api")
+
+const connectionEpic = (action$) => 
+  action$.filter( (action) => {
+    return action.meta && !!(action.meta.uuid)
+  }).map( (action) => {
+    return startConnection(action.meta.uuid)
+  })
+
+const createFinish = (action$) => 
+  action$.map( (action) => finishConnection(action.meta.uuid ) )
+
+const patchEpic = (action$, store) =>
+  action$.ofType("PATCH")
+    .mergeMap((action) => patchApi() )
+    .map( ({ data }) => {
+      return fullfiledAction(data.member)
+    })
+    .withLatestFrom(createFinish(action$))
+    .concatMap( ( a ) => a )
+
+
+describe("", () => {
+  it("4--", (done) => {
+    const initActionMock = { type: "@INIT"}
+    const action$ = ActionsObservable.of(
+      patchAction(),
+      patchAction()
+    )
+
+    const epic = combineEpics( 
+      connectionEpic,
+      patchEpic
+    )
+    const start = new Date().getTime()
+    epic(action$, {})
+      .subscribe( (r) => {
+        console.log((new Date().getTime() - start) ,r)
+      }, (e) => {} , (result) => {
+        done()
+      })
+  })
+})
