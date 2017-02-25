21bfef37f32df52c95458b5d524bb56206b255e1
inuscript <inuscript@gmail.com>
2017-02-25 13:35:30 +0900

    add

--- rx-custom-operator/test/index.ts
+++ rx-custom-operator/test/index.ts
@@ -1,3 +1,50 @@
 import "rxjs"
 import { patchApi } from "../api"
-import { ActionsObservable } from "redux-observable"
+import { ActionsObservable, combineEpics, Epic } from "redux-observable"
+import { Observable } from "rxjs"
+
+function mySimpleOperator(someCallback) {
+   return ActionsObservable.create(subscriber => {
+     console.log("HEEEE")
+     var source = this;
+
+     var subscription = source.subscribe(value => {
+       try {
+         subscriber.next(someCallback(value));
+       } catch(err) {
+         subscriber.error(err);
+       }
+     },
+     err => subscriber.error(err),
+     () => subscriber.complete());
+
+     // to return now
+     return subscription;
+   });
+}
+
+const patchEpicBase: Epic<any, any> = (action$, store) =>
+  action$.ofType("PATCH")
+    .mergeMap((action) => patchApi() )
+    .map( ({ data }) => ({type: "FULLFILL", data }) )
+
+const patch2Epic: Epic<any, any> = (action$, store) =>
+  action$.ofType("PATCH")
+    .lift(mySimpleOperator(a => console.log(a)))
+    .ignoreElements()
+
+const it = () => {
+  console.log("start")
+  const seedAction$ = ActionsObservable.of({type: "PATCh"})
+  const epics = combineEpics(patchEpicBase, patch2Epic)
+  epics(seedAction$, {})
+    .toArray()
+    .subscribe( r => {
+      console.log(r)
+    }, 
+    e => { throw e }, 
+    () => {
+      console.log("complete")
+    })
+}
+it()
