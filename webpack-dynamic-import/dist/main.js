3a0d0097686c0cc1293ffbcc745bbae7b2378307
inuscript <inuscript@gmail.com>
2017-04-29 18:48:28 +0900

    wip

---
+++ webpack-dynamic-import/dist/main.js
@@ -0,0 +1,18 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = () => {
+  console.log("This is Lib")
+}
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+const lib = __webpack_require__(0)
+
+lib()
+
+/***/ })
+],[1]);
