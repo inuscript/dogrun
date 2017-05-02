4aa304fd54fc2c3d44cda9c85a79d8e24842da2c
inuscript <inuscript@gmail.com>
2017-05-02 09:26:38 +0900

    bundle-lazy

--- webpack-dynamic-import/dist/main.js
+++ webpack-dynamic-import/dist/main.js
@@ -2,20 +2,11 @@ webpackJsonp([1],[
 /* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
-var cbs = [], 
-	data;
 module.exports = function(cb) {
-	if(cbs) cbs.push(cb);
-	else cb(data);
+	__webpack_require__.e/* require.ensure */(0).then((function(require) {
+		cb(__webpack_require__(2));
+	}).bind(null, __webpack_require__)).catch(__webpack_require__.oe);
 }
-__webpack_require__.e/* require.ensure */(0).then((function(require) {
-	data = __webpack_require__(2);
-	var callbacks = cbs;
-	cbs = null;
-	for(var i = 0, l = callbacks.length; i < l; i++) {
-		callbacks[i](data);
-	}
-}).bind(null, __webpack_require__)).catch(__webpack_require__.oe);
 
 /***/ }),
 /* 1 */
