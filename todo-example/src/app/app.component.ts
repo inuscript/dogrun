0bfc674788cb27158d63f3e04531ece52efdbba6
inuscript <inuscript@gmail.com>
2017-05-04 01:12:37 +0900

    add

--- todo-example/src/app/app.component.ts
+++ todo-example/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'app works!';
+// }
+
+
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello {{name}}</h1>`
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent { name = 'Angular'; }
