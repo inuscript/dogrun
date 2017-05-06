7b5abe73d8eb35db35d47f0bf232914fc72aa200
inuscript <inuscript@gmail.com>
2017-05-06 19:25:40 +0900

    add

--- redux-observable-sandbox/test/test1.js
+++ redux-observable-sandbox/test/test1.js
@@ -1,5 +1,7 @@
 require("rxjs")
-const { Observable, Subject } = require("rxjs")
+const { Observable } = require("rxjs")
+const assert = require("assert")
+
 const { ActionsObservable, combineEpics, createEpicMiddleware } = require("redux-observable")
 const configureMockStore = require('redux-mock-store').default
 const testSubject = require("./testHelper")
@@ -66,8 +68,8 @@ const rootEpic = combineEpics(
 const epicMiddleware = createEpicMiddleware(rootEpic);
 const mockStore = configureMockStore([epicMiddleware]);
 
-describe("",() => {
-  it("", (done) => {
+describe("Action chain sample",() => {
+  it("chain", (done) => {
     const initialActions = []
     const subject = testSubject(rootEpic)
 
@@ -82,7 +84,12 @@ describe("",() => {
       .toArray()
       .toPromise()
       .then( actions => {
-        console.log(actions)
+        const expect = [ 
+          { type: 'FETCH_AUTHOR_REQUEST', author: '夏目漱石' },
+          { type: 'FULLFILLED_BOOK', data: { id: 100, title: '吾輩は猫である', author: '夏目漱石' } },
+          { type: 'FULLFILLED_AUTHOR', data: { name: '夏目漱石', birthday: '1867-02-09' } } 
+        ]
+        assert.deepEqual(expect, actions)
         return done()
       })
   })
