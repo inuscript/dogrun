4fcdabdfe037b2d72e8a0bde84bcd1214200b1ba
inuscript <inuscript@gmail.com>
2017-07-01 15:14:18 +0900

    wiP

--- graphql-to-mysql/dd.js
+++ graphql-to-mysql/dd.js
@@ -1,1 +1,9 @@
-const User = sequelize.define('user');
+const { Sequelize } = require("sequelize")
+const sequelize = new Sequelize('database', '', '', {
+  // host: 'localhost',
+  dialect: 'sqlite',
+  // SQLite only
+  storage: 'path/to/test.sqlite'
+})
+
+sequelize.
--- react-video/src/App.js
+++ react-video/src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import YouTube from "react-youtube"
+// https://developers.google.com/youtube/player_parameters?playerVersion=HTML5
+const Part1 = (props) => (
+  <YouTube videoId="4GK1NDTWbkY" 
+    {...props}
+  />
+)
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Part1 opts={{
+          playerVars: {
+            start: 35,
+            end: 58,
+            rel: 0,
+            showinfo: 0,
+            controls: 2
+          }}}
+          onEnd={(e) => {
+            console.log(e)
+          } }
+        />
+        
       </div>
     );
   }
