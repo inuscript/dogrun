04ac28d64ef94151870c266673b207744bad63e6
inuscript <inuscript@gmail.com>
2017-10-01 06:42:14 +0900

    add

---
+++ vue-message/src/components/Chat.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div>hello</div>
+
+    <!-- message -->
+    <div class="msg-box" v-for="msg in messages">
+      <span>{{msg.user}}</span>:<span>{{msg.message}}</span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  // components: { MessageBox },
+  data: () => {
+    return {
+      messages: [{
+        id: 1,
+        user: "foo",
+        message: "Hello"
+      }]
+    }
+  }
+}
+</script>
+
