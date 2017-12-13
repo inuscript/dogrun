5cf877e996fcad922367efeed5ce4ce0528567db
inuscript <inuscript@gmail.com>
2017-12-13 10:11:52 +0900

    wip

---
+++ vue-v-model/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <CheckboxList
+      class="plan-selectbox"
+      name="service_type"
+      :options="items"
+      :selected="[1,3]"
+    />
+
+  </div>
+</template>
+
+<script>
+import CheckboxList from "./CheckboxList"
+export default {
+  name: 'app',
+  data(){
+    return {
+      items: [{
+        label: "yokie",
+        value: 1
+      },{
+        label: "samoyed",
+        value: 2
+      }, {
+        label: "norfolk",
+        value: 3
+      }]
+    }
+  },
+  components: {
+    CheckboxList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
