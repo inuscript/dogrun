f7ffa0b5bebef3614dced768f912eaec1663cd65
inuscript <inuscript@gmail.com>
2017-12-26 11:47:03 +0900

    add

--- redux-zero-observable/src/store.js
+++ redux-zero-observable/src/store.js
@@ -1,26 +1,4 @@
 import createStore from "redux-zero";
-<<<<<<< HEAD
-import { applyMiddleware } from "redux-zero/middleware"
-import { Subject } from 'rxjs/Subject'
-import { map } from 'rxjs/operators'
-
-const stream = (store) => (next) => {
-//   const input$ = new Subject()
-//   const epic$ = new Subject()
-//   epic$.pipe(
-//     map( () => {})
-//   )
-  return (action) => {
-    console.log(action.name)
-    return next(action);
-  }
-}
-
-const initialState = { count: 1 }
-const middleware = applyMiddleware(stream)
-
-const store = createStore(initialState, middleware)
-=======
 import { observableMiddleware } from "./observableMiddleware"
 // import { ofType } from "redux-observable"
 import { ofType } from "./ofType"
@@ -48,6 +26,5 @@ const middleware = observableMiddleware(epic)
 const store = createStore(initialState, applyMiddleware(
   middleware
 ))
->>>>>>> e3af0d08b1924d2bf2534515d4e5850df205180f
 
 export default store;
